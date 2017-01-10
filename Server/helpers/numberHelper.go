f7eacbb9f037ae2019d5b730f146843bcf85680d
poluhinse <poluhinse@gmail.com>
2017-01-10 17:19:49 +0300

    remove spam message from log.

--- Server/helpers/numberHelper.go
+++ Server/helpers/numberHelper.go
@@ -12,7 +12,6 @@ func ParceNumberAndLocalize(source string) string {
 	var str = strings.TrimSpace(source)
 	var value, err = strconv.ParseInt(str, 10, 64)
 	if err != nil {
-		lspLogger.Errorf("error: %v", err)
 		return source
 	}
 	return humanize.Comma(value)
